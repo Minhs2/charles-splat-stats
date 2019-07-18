b2c66ca43cb05e88ba3c423cd37134a2ff76e2ca
Minhs2 <32379779+Minhs2@users.noreply.github.com>
2019-07-17 18:16:43 -0700

    Added a few comments

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import urllib.request
 import time
 
 def fetchStats():
+    # Open a stripped down (htmlembed) version of the spreadsheet
     source = urllib.request.urlopen('https://docs.google.com/spreadsheets/d/1di9JvURVglp10jakbaZoVBfy9xFWnJtiIVbU5QH94Q4/htmlembed/sheet?gid=1419716716').read()
 
     soup = beausoup.BeautifulSoup(source,'lxml')
     numbersArr = []
 
+    # Fetch the cells from the url
     for numbers in soup.find_all('td'):
         numbersArr.append(numbers.text)
         
+    # Concatenate and format string
     finalStr = " | Games vs Order: " + numbersArr[0] + " | Win% vs Order: " + numbersArr[1] + " | Games vs Chaos: " + numbersArr[2] + " | Win% vs Chaos: " + numbersArr[3]
+    # Write to file
     file = open("OBSOutput.txt","w")
     file.write(finalStr)
     file.close()    
     
 def main():
+    # Run fetchStats() until forcefully terminated
     while True:
         fetchStats()
         time.sleep(10)
